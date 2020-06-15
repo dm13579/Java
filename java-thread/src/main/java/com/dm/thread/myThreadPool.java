24740e5caf5606ca91006b2458a5b899665dc597
dm13579 <7028858dm>
2020-06-15 23:49:34 +0800

    maven改造

--- java-thread/src/main/java/com/dm/thread/myThreadPool.java
+++ java-thread/src/main/java/com/dm/thread/myThreadPool.java
@@ -45,14 +45,17 @@ public class myThreadPool {
             namePrefix = "mythread-";
         }
 
-        public Thread newThread(Runnable r) {
+        @Override
+		public Thread newThread(Runnable r) {
             Thread t = new Thread(group, r,
                                   namePrefix + threadNumber.getAndIncrement(),
                                   0);
-            if (t.isDaemon())
-                t.setDaemon(false);
-            if (t.getPriority() != Thread.NORM_PRIORITY)
-                t.setPriority(Thread.NORM_PRIORITY);
+            if (t.isDaemon()) {
+				t.setDaemon(false);
+			}
+            if (t.getPriority() != Thread.NORM_PRIORITY) {
+				t.setPriority(Thread.NORM_PRIORITY);
+			}
             return t;
         }
     }
